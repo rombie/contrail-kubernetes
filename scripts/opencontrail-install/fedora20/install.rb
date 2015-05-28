d917615efbd8ab65a6fbc3986d3b9f4e43b8918e
Ananth Suryanarayana <anantha@juniper.net>
2015-05-28 11:52:01 -0700

    Ignore docker restart command exit status

--- scripts/opencontrail-install/fedora20/install.rb
+++ scripts/opencontrail-install/fedora20/install.rb
@@ -153,7 +153,7 @@ end
 # Install third-party software
 def install_thirdparty_software_compute
     sh("yum -y install #{@compute_thirdparty_packages.join(" ")}")
-    sh("service docker restart")
+    sh("service docker restart", true)
 #   sh("docker pull ubuntu")
 end
 
