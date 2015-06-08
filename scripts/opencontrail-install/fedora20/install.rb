43907e410f1553c9d330fe9b64f8387619687478
Ananth Suryanarayana <anantharamu@gmail.com>
2015-06-08 11:04:58 -0700

    Add nodejs souce

--- scripts/opencontrail-install/fedora20/install.rb
+++ scripts/opencontrail-install/fedora20/install.rb
@@ -108,6 +108,7 @@ end
 # Install from /cs-shared/builder/cache/centoslinux70/juno
 def install_thirdparty_software_controller
     sh("yum -y remove java-1.8.0-openjdk java-1.8.0-openjdk-headless")
+    sh("curl -sL https://rpm.nodesource.com/setup | bash -")
     sh("yum -y install #{@controller_thirdparty_packages.join(" ")}")
 end
 
