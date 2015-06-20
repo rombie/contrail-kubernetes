d18089f50816f25c20d210862b6db3475e8d9d55
Ananth Suryanarayana <anantha@juniper.net>
2015-06-20 11:59:55 -0700

    reuse kubernetes sandbox

--- scripts/opencontrail-install/ubuntu1404/install.rb
+++ scripts/opencontrail-install/ubuntu1404/install.rb
@@ -110,8 +110,8 @@ def install_contrail_software_compute
     sh("add-apt-repository -y ppa:opencontrail/ppa")
     sh("add-apt-repository -y ppa:anantha-l/opencontrail")
     sh("apt-get -y --allow-unauthenticated update")
-    sh("dpkg --force-all -i /home/ubuntu/contrail-vrouter-init_3.0-2615_all.deb")
     sh("apt-get -y --allow-unauthenticated install contrail-vrouter-agent contrail-utils python-contrail-vrouter-api")
+    sh("dpkg --force-all -i /home/ubuntu/contrail-vrouter-init_3.0-2615_all.deb")
 
     # Update time-zone
     sh("echo 'America/Los_Angeles' > /etc/timezone")
