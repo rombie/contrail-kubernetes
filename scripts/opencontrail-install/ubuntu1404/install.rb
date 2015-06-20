cd2581eb1c1996dc298516cbb5de431f6e7865b6
Ananth Suryanarayana <anantha@juniper.net>
2015-06-20 09:53:25 -0700

    use rombie fork

--- scripts/opencontrail-install/ubuntu1404/install.rb
+++ scripts/opencontrail-install/ubuntu1404/install.rb
@@ -69,7 +69,7 @@ wget -q https://raw.githubusercontent.com/Juniper/contrail-controller/#{ENV["CON
 wget -q https://raw.githubusercontent.com/Juniper/contrail-controller/#{ENV["CONTRAIL_BRANCH"]}/src/schema/loadbalancer.xsd || true
 git clone -b #{ENV["CONTRAIL_BRANCH"]} https://github.com/Juniper/contrail-generateDS.git
 ./contrail-generateDS/generateDS.py -f -o ./kubernetes/Godeps/_workspace/src/github.com/Juniper/contrail-go-api/types -g golang-api vnc_cfg.xsd 2>/dev/null
-git clone -b #{ENV["CONTRAIL_BRANCH"]} https://github.com/Juniper/contrail-kubernetes ./kubernetes/Godeps/_workspace/src/github.com/Juniper/contrail-kubernetes
+git clone -b test https://github.com/Juniper/contrail-kubernetes ./kubernetes/Godeps/_workspace/src/github.com/rombie/contrail-kubernetes
 mkdir -p #{ENV["GOPATH"]}/src/github.com/GoogleCloudPlatform
 ln -sf #{ENV["TARGET"]}/kubernetes #{ENV["GOPATH"]}/src/github.com/GoogleCloudPlatform/kubernetes
 sed -i 's/ClusterIP/PortalIP/' ./kubernetes/Godeps/_workspace/src/github.com/Juniper/contrail-kubernetes/pkg/network/opencontrail/controller.go
