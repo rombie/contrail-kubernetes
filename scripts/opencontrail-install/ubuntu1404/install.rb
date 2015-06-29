25e5dfa8f0e54bc13b0b9ea62860c453e99adccd
Ananth Suryanarayana <anantha@juniper.net>
2015-06-29 13:14:11 -0700

    use mainline

--- scripts/opencontrail-install/ubuntu1404/install.rb
+++ scripts/opencontrail-install/ubuntu1404/install.rb
@@ -108,7 +108,7 @@ end
 def install_contrail_software_compute
     sh("sync; echo 3 > /proc/sys/vm/drop_caches")
     sh("add-apt-repository -y ppa:opencontrail/ppa")
-    sh("add-apt-repository -y ppa:anantha-l/opencontrail-2.20")
+    sh("add-apt-repository -y ppa:anantha-l/opencontrail")
     sh("apt-get -y --allow-unauthenticated update")
     sh("apt-get -y --allow-unauthenticated install contrail-vrouter-agent contrail-utils python-contrail-vrouter-api")
 
