171deb8fa5d921d2eb4d834128713c654c7db4d3
Ananth Suryanarayana <anantha@juniper.net>
2015-06-30 00:47:40 -0700

    update ppa

--- scripts/opencontrail-install/ubuntu1404/install.rb
+++ scripts/opencontrail-install/ubuntu1404/install.rb
@@ -111,7 +111,7 @@ def install_contrail_software_compute
     sh("add-apt-repository -y ppa:opencontrail/ppa")
     sh("add-apt-repository -y ppa:anantha-l/opencontrail-2.20")
     sh("apt-get -y --allow-unauthenticated update")
-    sh("apt-get -y --allow-unauthenticated install contrail-vrouter-agent contrail-vrouter-utils contrail-utils")
+    sh("apt-get -y --allow-unauthenticated install contrail-vrouter-agent contrail-vrouter-utils contrail-utils python-contrail-vrouter-api")
 
     # Install contrail-vrouter-init and contrail-setup packages also
     sh("gdebi -n /home/ubuntu/contrail-vrouter-init_*.deb")
